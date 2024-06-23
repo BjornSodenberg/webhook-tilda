6adfcab14ddc63d16b9bb1fc8d1cb00c9f85028a
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2024-06-23 21:47:43 +0700

    tilda webhook

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const fns = require("date-fns");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+app.use(bodyParser.json());
+
+app.get("/webhook", async (req, res) => {
+  res.status(200).send("Webhook – ok");
+  console.log("Webhook – ok");
+})
+
+app.post("/webhook", async (req, res) => {
+  const orderData = req.body;
+
+  const transactionItem = {
+    count: orderData.payment.amount,
+    date: fns.format(new Date(Date.now(), "yyyy-MM-dd hh:mm")),
+    email: orderData.ma_email,
+    id: orderData.payment.orderId,
+    items: orderData.payment.products.map((p) => {
+      if (p.options) {
+        // "Футболка Зимний ЗаХод 2024 (Размер: L) – 1x350 ≡ 350"
+        return `${p.name} (${p.options.option}: ${p.options.variant}) – ${p.quantity}X${p.price}=${p.amount};`;
+      }
+      // "Футболка Зимний ЗаХод 2024 – 1x350 ≡ 350"
+      return `${p.name} – ${p.quantity}X${p.price}=${p.amount};`;
+    }),
+  };
+
+  try {
+    const docRef = await db.collection("transactions").add(transactionItem);
+    console.log("Добавлен заказ с ID:", docRef.id);
+
+    res.status(200).send("Заказ получен и сохранен успешно");
+  } catch (error) {
+    console.error("Ошибка при сохранении заказа:", error);
+
+    res.status(500).send("Ошибка при сохранении заказа");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
+
