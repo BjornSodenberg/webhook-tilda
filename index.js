40f46f3a929fabb07ae26a1734a2c93895f89804
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2024-06-23 22:48:04 +0700

    tilda webhook

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-const { format } = require('date-fns');
+const { format } = require("date-fns");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,32 +27,32 @@ app.post("/webhook", async (req, res) => {
   // Проверка на данные { test: "test" }
   if (orderData.test === "test") {
     return res.status(200).send("Test data received");
-  }
+  } else {
+    const transactionItem = {
+      count: orderData.payment.amount,
+      date: format(new Date(), "yyyy-MM-dd hh:mm"),
+      email: orderData.ma_email,
+      id: orderData.payment.orderId,
+      items: orderData.payment.products.map((p) => {
+        if (p.options) {
+          // "Футболка Зимний ЗаХод 2024 (Размер: L) – 1x350 ≡ 350"
+          return `${p.name} (${p.options.option}: ${p.options.variant}) – ${p.quantity}x${p.price}=${p.amount};`;
+        }
+        // "Футболка Зимний ЗаХод 2024 – 1x350 ≡ 350"
+        return `${p.name} – ${p.quantity}x${p.price}=${p.amount};`;
+      }),
+    };
 
-  const transactionItem = {
-    count: orderData.payment.amount,
-    date: format(new Date(), "yyyy-MM-dd hh:mm"),
-    email: orderData.ma_email,
-    id: orderData.payment.orderId,
-    items: orderData.payment.products.map((p) => {
-      if (p.options) {
-        // "Футболка Зимний ЗаХод 2024 (Размер: L) – 1x350 ≡ 350"
-        return `${p.name} (${p.options.option}: ${p.options.variant}) – ${p.quantity}x${p.price}=${p.amount};`;
-      }
-      // "Футболка Зимний ЗаХод 2024 – 1x350 ≡ 350"
-      return `${p.name} – ${p.quantity}x${p.price}=${p.amount};`;
-    }),
-  };
+    try {
+      const docRef = await db.collection("transactions").add(transactionItem);
+      console.log("Добавлен заказ с ID:", docRef.id);
 
-  try {
-    const docRef = await db.collection("transactions").add(transactionItem);
-    console.log("Добавлен заказ с ID:", docRef.id);
+      res.status(200).send("Заказ получен и сохранен успешно");
+    } catch (error) {
+      console.error("Ошибка при сохранении заказа:", error);
 
-    res.status(200).send("Заказ получен и сохранен успешно");
-  } catch (error) {
-    console.error("Ошибка при сохранении заказа:", error);
-
-    res.status(500).send("Ошибка при сохранении заказа");
+      res.status(500).send("Ошибка при сохранении заказа");
+    }
   }
 });
 
